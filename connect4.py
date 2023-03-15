6b6389122872bf41697bc00c9a4d5f90a6b2c474
gbman <chun.h.man@gmail.com>
2023-03-16 00:37:20 +1100

    half way to task 6

--- connect4.py
+++ connect4.py
@@ -4,7 +4,7 @@ board = [
     [0, 0, 0, 0, 0, 0, 0],
     [1, 0, 0, 0, 0, 0, 0],
     [1, 0, 0, 0, 0, 0, 0],
-    [1, 2, 2, 2, 0, 0, 0]
+    [1, 2, 2, 2, 2, 0, 0]
 ]
 zero_arr = []
 one_arr = []
@@ -19,32 +19,32 @@ num = 0
 
 
 while num < len(board):
-
+    # print(len(board))
     i = board[num]
-    for index in i:
-        index = 0
-        index_in_i = 0
+    index_in_i = 0
 
-        # li = []
-        while index < len(i):
+    # li = []
+    while index_in_i < len(i):
+        # print (len(i))
+        if i[index_in_i] == 1:
+            # li.append(index)
+            if i[index_in_i] == i[index_in_i+1] == i[index_in_i+2] == i[index_in_i+3]:
+                print(1)
+            # li.clear()
+        elif i[index_in_i] == 2:
+            # li.append(index)
+            if i[index_in_i] == i[index_in_i+1] == i[index_in_i+2] == i[index_in_i+3]:
+                print(2)
 
-            if i[index] == 1:
-                # li.append(index)
+            # li.clear()
 
-                if i[index_in_i] == i[index_in_i+1] == i[index_in_i+2] == i[index_in_i+3]:
-                    print(1)
-                # li.clear()
-            elif i[index] == 2:
-                # li.append(index)
-                if li[index_in_i] == li[index_in_i+1] == li[index_in_i+2] == li[index_in_i+3]:
-                    print(2)
-                # li.clear()
+        index_in_i += 1
+        # print(index_in_i)
 
-        index +=1
-
-
-    if index == 0 and len(i)> index_vertical:
-        zero_arr.append(i[index])
+    num = num + 1
+    print(num)
+    # if index == 0 and len(i)> index_vertical:
+    #     zero_arr.append(i[index])
 
 
 
@@ -65,7 +65,7 @@ while num < len(board):
     #     elif index_vertical == 2:
     #         two_arr.append(i[index_vertical])
 
-    num +=1
+
 
 
 
